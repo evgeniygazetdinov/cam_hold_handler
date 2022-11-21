9ef23bf45b2e7a78add0422a9d92f178bd4e11e9
Евгений Газетдинов <e.gazetdinov@litebox.ru>
2022-11-21 05:43:59 +0300

    add const file and some reformating TODO SAVE PHOTO in folder

--- crud.py
+++ crud.py
@@ -3,16 +3,19 @@ from db import DB as db
 
 
 def add_book(title):
-        book = Book(title=title)
-        db.session.add(book)
-        db.session.commit()
+    book = Book(title=title)
+    db.session.add(book)
+    db.session.commit()
+
 
 def one_book_by_title(title):
     pass
 
+
 def create():
     db.create_all()
     db.commit()
+
 
 def all_books():
     return Book.query.all()
--- main.py
+++ main.py
@@ -1,33 +1,68 @@
-from flask import Flask,render_template,request,redirect, Response
-from models import db,EmployeeModel
+from flask import render_template, request, redirect, Response
+from models import db, EmployeeModel
 import cv2
+from werkzeug.utils import secure_filename
+from const import (
+    make_flask_app,
+    ALLOWED_EXTENSIONS,
+    PATH_TO_YOUR_FOLDER,
+    CAMERA_ADDRESS,
+)
 
-app = Flask(__name__)
-addres = 'rtsp://192.168.1.103/live/ch00_0'
-
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app = make_flask_app()
 db.init_app(app)
-count = 1
+
 
 def render_picture(data):
 
-    render_pic = base64.b64encode(data).decode('ascii') 
+    render_pic = base64.b64encode(data).decode("ascii")
     return render_pic
 
 
+#############experemental#####################
+
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/upload-my-image", methods=["POST"])
+def upload_file():
+    # check if the post request has the file part
+    if "file" not in request.files:
+        flash("No file part")
+        return redirect(request.url)
+    file = request.files["file"]
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == "":
+        flash("No selected file")
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(ABSOLUTE_PATH_TO_YOUR_FOLDER, filename))
+        new_image = Image(
+            path=PATH_TO_YOUR_FOLDER,
+            filename=filename,
+            ext=filename.rsplit(".", 1)[1].lower(),
+        )
+        # Save new_image model
+
+
+#############experemental#####################
 
 
 @app.before_first_request
 def create_table():
     db.create_all()
 
+
 def gen_frames():  # generate frame by frame from camera
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades 
-                                     + 'haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+    )
     video_capture = cv2.VideoCapture(0)
-    # cv2.VideoCapture(addres) 
+    # cv2.VideoCapture(addres)
     while True:
         # Capture frame-by-frame
         success, frame = video_capture.read()  # read the camera frame
@@ -37,103 +72,108 @@ def gen_frames():  # generate frame by frame from camera
             ret, frame = video_capture.read()
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             faces = face_cascade.detectMultiScale(
-                                                gray,
-                                                scaleFactor=1.3,
-                                                minNeighbors=5,
-                                                minSize=(100, 100),
-                                                flags=cv2.CASCADE_SCALE_IMAGE
-                                            )
+                gray,
+                scaleFactor=1.3,
+                minNeighbors=5,
+                minSize=(100, 100),
+                flags=cv2.CASCADE_SCALE_IMAGE,
+            )
 
             for (x, y, w, h) in faces:
-            # for each face on the image detected by OpenCV
-            # draw a rectangle around the face
-                cv2.rectangle(frame, 
-                            (x, y), # start_point
-                            (x+w, y+h), # end_point
-                            (255, 0, 0),  # color in BGR
-                            2) # thickness in px
-            ret, buffer = cv2.imencode('.jpg', frame)
+                # for each face on the image detected by OpenCV
+                # draw a rectangle around the face
+                cv2.rectangle(
+                    frame,
+                    (x, y),  # start_point
+                    (x + w, y + h),  # end_point
+                    (255, 0, 0),  # color in BGR
+                    2,
+                )  # thickness in px
+            ret, buffer = cv2.imencode(".jpg", frame)
             frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-@app.route('/1')
+            yield (
+                b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
+            )  # concat frame one by one and show result
+
+
+@app.route("/1")
 def video_feed():
-    #Video streaming route. Put this in the src attribute of an img tag
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/upload/<str:byte_foto>', methods=['POST'])
-def make_photo(byte_photo):
-    f = open('1.jpg', 'wb')
-    f.write(byte_photo)
-    f.close()
-    return render_template('index.html', count=0)  
+    # Video streaming route. Put this in the src attribute of an img tag
+    return Response(gen_frames(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
-@app.route('/')
+@app.route("/")
 def main():
-    #Video streaming route. Put this in the src attribute of an img tag
-    return render_template('index.html', count=0)   
+    # Video streaming route. Put this in the src attribute of an img tag
+    return render_template("index.html", count=0)
 
-@app.route('/data/create' , methods = ['GET','POST'])
+
+@app.route("/data/create", methods=["GET", "POST"])
 def create():
-    if request.method == 'GET':
-        return render_template('createpage.html')
+    if request.method == "GET":
+        return render_template("createpage.html")
 
-    if request.method == 'POST':
-        employee_id = request.form['employee_id']
-        name = request.form['name']
-        age = request.form['age']
-        position = request.form['position']
-        employee = EmployeeModel(employee_id=employee_id, name=name, age=age, position = position)
+    if request.method == "POST":
+        employee_id = request.form["employee_id"]
+        name = request.form["name"]
+        age = request.form["age"]
+        position = request.form["position"]
+        employee = EmployeeModel(
+            employee_id=employee_id, name=name, age=age, position=position
+        )
         db.session.add(employee)
         db.session.commit()
-        return redirect('/data')
+        return redirect("/data")
 
 
-@app.route('/data')
+@app.route("/data")
 def RetrieveList():
     employees = EmployeeModel.query.all()
     print(employees)
-    return render_template('datalist.html',employees = employees)
+    return render_template("datalist.html", employees=employees)
 
 
-@app.route('/data/<int:id>')
+@app.route("/data/<int:id>")
 def RetrieveEmployee(id):
     employee = EmployeeModel.query.filter_by(employee_id=id).first()
     if employee:
-        return render_template('data.html', employee = employee)
+        return render_template("data.html", employee=employee)
     return f"Employee with id ={id} Doenst exist"
 
 
-@app.route('/data/<int:id>/update',methods = ['GET','POST'])
+@app.route("/data/<int:id>/update", methods=["GET", "POST"])
 def update(id):
     employee = EmployeeModel.query.filter_by(employee_id=id).first()
-    if request.method == 'POST':
+    if request.method == "POST":
         if employee:
             db.session.delete(employee)
             db.session.commit()
-            name = request.form['name']
-            age = request.form['age']
-            position = request.form['position']
-            employee = EmployeeModel(employee_id=id, name=name, age=age, position = position)
+            name = request.form["name"]
+            age = request.form["age"]
+            position = request.form["position"]
+            employee = EmployeeModel(
+                employee_id=id, name=name, age=age, position=position
+            )
             db.session.add(employee)
             db.session.commit()
-            return redirect(f'/data/{id}')
+            return redirect(f"/data/{id}")
         return f"Employee with id = {id} Does nit exist"
 
-    return render_template('update.html', employee = employee)
+    return render_template("update.html", employee=employee)
 
 
-@app.route('/data/<int:id>/delete', methods=['GET','POST'])
+@app.route("/data/<int:id>/delete", methods=["GET", "POST"])
 def delete(id):
     employee = EmployeeModel.query.filter_by(employee_id=id).first()
-    if request.method == 'POST':
+    if request.method == "POST":
         if employee:
             db.session.delete(employee)
             db.session.commit()
-            return redirect('/data')
+            return redirect("/data")
         abort(404)
 
-    return render_template('delete.html')
+    return render_template("delete.html")
 
-app.run(host='0.0.0.0', port=5001)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
