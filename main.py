14e17530ecc136c3a7889c25578f0176544a9dfd
Евгений Газетдинов <e.gazetdinov@litebox.ru>
2022-11-22 04:03:18 +0300

    add first implement photo TODO work superfluos photo on first loop

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import os
+import datetime
+import time
+from threading import Thread
+
 from flask import render_template, request, redirect, Response
 from models import db, EmployeeModel
 import cv2
@@ -11,51 +16,74 @@ from const import (
 
 app = make_flask_app()
 db.init_app(app)
+global capture,rec_frame, grey, switch, neg, face, rec, out 
+capture=0
+grey=0
+neg=0
+face=0
+switch=1
+rec=0
+try:
+    os.mkdir('./shots')
+except OSError as error:
+    pass
 
 
-def render_picture(data):
-
-    render_pic = base64.b64encode(data).decode("ascii")
-    return render_pic
-
-
-#############experemental#####################
-
-
-def allowed_file(filename):
-    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-@app.route("/upload-my-image", methods=["POST"])
-def upload_file():
-    # check if the post request has the file part
-    if "file" not in request.files:
-        flash("No file part")
-        return redirect(request.url)
-    file = request.files["file"]
-    # if user does not select file, browser also
-    # submit an empty part without filename
-    if file.filename == "":
-        flash("No selected file")
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(ABSOLUTE_PATH_TO_YOUR_FOLDER, filename))
-        new_image = Image(
-            path=PATH_TO_YOUR_FOLDER,
-            filename=filename,
-            ext=filename.rsplit(".", 1)[1].lower(),
-        )
-        # Save new_image model
-
-
-#############experemental#####################
-
+@app.route('/requests',methods=['POST','GET'])
+def tasks():
+    global switch,camera
+    if request.method == 'POST':
+        if request.form.get('click') == 'Capture':
+            global capture
+            capture=1
+        elif  request.form.get('grey') == 'Grey':
+            global grey
+            grey=not grey
+        elif  request.form.get('neg') == 'Negative':
+            global neg
+            neg=not neg
+        elif  request.form.get('face') == 'Face Only':
+            global face
+            face=not face 
+            if(face):
+                time.sleep(4)   
+        elif  request.form.get('stop') == 'Stop/Start':
+            
+            if(switch==1):
+                switch=0
+                camera.release()
+                cv2.destroyAllWindows()
+                
+            else:
+                camera = cv2.VideoCapture(0)
+                switch=1
+        elif  request.form.get('rec') == 'Start/Stop Recording':
+            global rec, out
+            rec= not rec
+            if(rec):
+                now=datetime.datetime.now() 
+                fourcc = cv2.VideoWriter_fourcc(*'XVID')
+                out = cv2.VideoWriter('vid_{}.avi'.format(str(now).replace(":",'')), fourcc, 20.0, (640, 480))
+                #Start new thread for recording the video
+                thread = Thread(target = record, args=[out,])
+                thread.start()
+            elif(rec==False):
+                out.release()
+                          
+                 
+    elif request.method=='GET':
+        return render_template('index2.html')
+    return render_template('index2.html')
 
 @app.before_first_request
 def create_table():
     db.create_all()
 
+# def record(out):
+#     global rec_frame
+#     while(rec):
+#         time.sleep(0.05)
+#         out.write(rec_frame)
 
 def gen_frames():  # generate frame by frame from camera
     face_cascade = cv2.CascadeClassifier(
@@ -63,6 +91,7 @@ def gen_frames():  # generate frame by frame from camera
     )
     video_capture = cv2.VideoCapture(0)
     # cv2.VideoCapture(addres)
+    global out, capture,rec_frame
     while True:
         # Capture frame-by-frame
         success, frame = video_capture.read()  # read the camera frame
@@ -78,6 +107,16 @@ def gen_frames():  # generate frame by frame from camera
                 minSize=(100, 100),
                 flags=cv2.CASCADE_SCALE_IMAGE,
             )
+            if(capture):
+                print('here')
+                capture=0
+                now = datetime.datetime.now()
+                p = os.path.sep.join(['shots', "shot_{}.png".format(str(now).replace(":",''))])
+                cv2.imwrite(p, frame)
+            # if(rec):
+            #     rec_frame=frame
+            #     frame= cv2.putText(cv2.flip(frame,1),"Recording...", (0,25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),4)
+            #     frame=cv2.flip(frame,1)
 
             for (x, y, w, h) in faces:
                 # for each face on the image detected by OpenCV
